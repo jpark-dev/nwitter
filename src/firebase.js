b25431dbfe0250400fcb7624d2e2add23c172685
Jasom Park <zeipar@gmail.com>
2020-09-20 19:06:33 -0700

    Installed firebase as a npm module and created firebase.js to store config and export as a module.:

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxFr1gk7uY9lTeHmIXp6rFYl6jCoWy2rE",
+  authDomain: "nwitter-6b8c1.firebaseapp.com",
+  databaseURL: "https://nwitter-6b8c1.firebaseio.com",
+  projectId: "nwitter-6b8c1",
+  storageBucket: "nwitter-6b8c1.appspot.com",
+  messagingSenderId: "59719246072",
+  appId: "1:59719246072:web:fa856d6b842073b4385b0d",
+};
+
+export default firebase.initializeApp(firebaseConfig);
