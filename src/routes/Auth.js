fcbc304724447e78aaa23990b419105ef552bdd3
Jasom Park <zeipar@gmail.com>
2020-09-21 13:43:00 -0700

    Implemented user Auth logic if submited.

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,17 +1,41 @@
+import { authService } from "myFirebase";
 import React, { useState } from "react";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [newAccount, setNewAccouhnt] = useState(true);
   const onChange = (e) => {
     console.log(e.target.name);
+    const {
+      target: { name, value },
+    } = e;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
   };
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await authService.createUserWithEmailAndPassword(
+          email,
+          password
+        );
+      } else {
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <input
           type="text"
           name="email"
@@ -28,7 +52,7 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Login" />
+        <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
       </form>
       <div>
         <button>Continue with Google</button>
