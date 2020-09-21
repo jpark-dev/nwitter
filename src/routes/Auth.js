d6be8ca91acdce3165715caa2d731c03002c64e0
Jasom Park <zeipar@gmail.com>
2020-09-21 14:31:26 -0700

    Added buttons and logic for login with socials with firebase.

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "myFirebase";
+import { authService, firebaseInstance } from "myFirebase";
 import React, { useState } from "react";
 
 const Auth = () => {
@@ -7,7 +7,6 @@ const Auth = () => {
   const [newAccount, setNewAccount] = useState(true);
   const [error, setError] = useState("");
   const onChange = (e) => {
-    console.log(e.target.name);
     const {
       target: { name, value },
     } = e;
@@ -37,6 +36,19 @@ const Auth = () => {
 
   const toggleAccount = () => {
     setNewAccount((prev) => !prev);
+  };
+  const onSocialClick = async (e) => {
+    const {
+      target: { name },
+    } = e;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    const data = await authService.signInWithPopup(provider);
+    console.log(data);
   };
 
   return (
@@ -68,8 +80,12 @@ const Auth = () => {
         {newAccount ? "Sign in" : "Create Account"}
       </span>
       <div>
-        <button>Continue with Google</button>
-        <button>Continue with Github</button>
+        <button name="google" onClick={onSocialClick}>
+          Continue with Google
+        </button>
+        <button name="github" onClick={onSocialClick}>
+          Continue with Github
+        </button>
       </div>
     </div>
   );
