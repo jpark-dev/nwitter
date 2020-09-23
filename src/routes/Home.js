f96202af1dea15fd3c8e10993cf88a2e77b32470
Jasom Park <zeipar@gmail.com>
2020-09-22 19:18:57 -0700

    Created simple submit form and created onSubmit function and linked with preventDefault().

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,17 @@
 import React from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  const onChange = (e) => {};
+  return (
+    <div>
+      <form onSumbit={onSubmit}>
+        <input type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="submit" value="Nweet" />
+      </form>
+    </div>
+  );
+};
 export default Home;
