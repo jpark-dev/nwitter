e12dcff3ee70ac8966f3aa45eebf0226d7a3f0c1
Jasom Park <zeipar@gmail.com>
2020-09-22 19:21:48 -0700

    Added tweet state and created onChange function to assign the text value.

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [nweet, setNweet] = useState("");
   const onSubmit = (e) => {
     e.preventDefault();
   };
-  const onChange = (e) => {};
+  const onChange = (e) => {
+    const {
+      target: { value },
+    } = e;
+    setNweet(value);
+  };
   return (
     <div>
       <form onSumbit={onSubmit}>
