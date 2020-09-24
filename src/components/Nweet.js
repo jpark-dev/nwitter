d1f996df1a43b947f156bafcc9bb9dedbcd385bf
Jasom Park <zeipar@gmail.com>
2020-09-23 19:38:47 -0700

    Delete nweet with document id of firebase.

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,14 +1,25 @@
+import { dbService } from "myFirebase";
 import React from "react";
 
-const Nweet = ({ nweetObj, isOwner }) => (
-  <div>
-    <h4>{nweetObj.text}</h4>
-    {isOwner && (
-      <>
-        <button>Delete</button>
-        <button>Edit</button>
-      </>
-    )}
-  </div>
-);
+const Nweet = ({ nweetObj, isOwner }) => {
+  const onDeleteClick = () => {
+    const proceedDelete = window.confirm("Delete this nweet?");
+
+    if (proceedDelete) {
+      dbService.doc(`nweets/${nweetObj.id}`).delete();
+    }
+  };
+
+  return (
+    <div>
+      <h4>{nweetObj.text}</h4>
+      {isOwner && (
+        <>
+          <button onClick={onDeleteClick}>Delete</button>
+          <button>Edit</button>
+        </>
+      )}
+    </div>
+  );
+};
 export default Nweet;
