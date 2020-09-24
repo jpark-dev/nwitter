234297088da814cfc7739a2d2b2ff8592382273f
Jasom Park <zeipar@gmail.com>
2020-09-23 19:34:13 -0700

    Shows button only if the userObj.uid equals to creatorId.

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-const Nweet = ({ nweetObj }) => (
+const Nweet = ({ nweetObj, isOwner }) => (
   <div>
     <h4>{nweetObj.text}</h4>
+    {isOwner && (
+      <>
+        <button>Delete</button>
+        <button>Edit</button>
+      </>
+    )}
   </div>
 );
 export default Nweet;
