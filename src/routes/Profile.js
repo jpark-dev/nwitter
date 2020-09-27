1b3a4950cad710c131a47cab5d748769573f2444
Jasom Park <zeipar@gmail.com>
2020-09-27 13:08:33 -0700

    Passed userObj to profile, created a query to lookup firebase DB with userObj.uid to only show current user's nweets.

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,26 @@
-import { authService } from "myFirebase";
-import React from "react";
+import { authService, dbService } from "myFirebase";
+import React, { useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
-export default () => {
+export default ({ userObj }) => {
   const history = useHistory();
   const onClick = () => {
     authService.signOut();
     history.push("/");
   };
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+
+    console.log(nweets.docs.map(doc => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
       <button onClick={onClick}>Log Out</button>
