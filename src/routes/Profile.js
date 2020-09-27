40c02963136ed3740cdf0f3709369fbffd40f866
Jasom Park <zeipar@gmail.com>
2020-09-27 13:46:38 -0700

    Added state:displayName and added a form to update it using firebase's updateProfile method.

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,9 +1,11 @@
 import { authService, dbService } from "myFirebase";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 export default ({ userObj }) => {
   const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
   const onClick = () => {
     authService.signOut();
     history.push("/");
@@ -17,12 +19,38 @@ export default ({ userObj }) => {
 
     console.log(nweets.docs.map(doc => doc.data()));
   };
+
+  const onChange = e => {
+    const {
+      target: { value },
+    } = e;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+    }
+  };
+
   useEffect(() => {
     getMyNweets();
   }, []);
 
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display Name"
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onClick}>Log Out</button>
     </>
   );
