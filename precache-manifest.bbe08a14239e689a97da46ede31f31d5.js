e8f73a4cab8cd3b8140153be966bce03ede63f64
Jasom Park <zeipar@gmail.com>
2020-10-01 16:09:28 -0700

    Updates

---
+++ precache-manifest.bbe08a14239e689a97da46ede31f31d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8961827dc6107719d3d180df7875318",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "1f58664dfd6fc284b6ad",
+    "url": "/nwitter/static/css/main.51c00765.chunk.css"
+  },
+  {
+    "revision": "a34e8ffc08e0a579803c",
+    "url": "/nwitter/static/js/2.d6dde593.chunk.js"
+  },
+  {
+    "revision": "2c0b89fe32f6d0b561deea7159cbd810",
+    "url": "/nwitter/static/js/2.d6dde593.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f58664dfd6fc284b6ad",
+    "url": "/nwitter/static/js/main.b3214424.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
